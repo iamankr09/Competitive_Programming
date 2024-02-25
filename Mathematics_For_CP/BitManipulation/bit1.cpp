87f44f2b85d49c70d999f3b92f4861a8d9a3f30b
iamankr09 <iamankr09@gmail.com>
2024-02-25 12:16:16 +0530

    cp commit

---
+++ Mathematics_For_CP/BitManipulation/bit1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int charTodigit(char ch){
+     return ch - '0';
+}
+char digitTochar(int num){
+    return  num + '0';
+}
